90a0b8a466b20251acd02fdd939837a53fb48037
gopn <gopinath.n@nokia.com>
2020-10-28 17:24:52 +0530

    init commit

---
+++ src/main/java/com/nithi/ngabui/NgabUiApplication.java
@@ -0,0 +1,13 @@
+package com.nithi.ngabui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgabUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgabUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nithi/ngabui/NgabUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nithi.ngabui;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class NgabUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertTrue(true);
+	}
+
+}
